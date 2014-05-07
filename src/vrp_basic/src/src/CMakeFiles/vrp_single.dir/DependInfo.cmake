4be304bc6ff5767a28f29f60a35daffd9725bf5b
manikanta-kondeti <mani.iiit123@gmail.com>
2014-05-07 10:44:15 +0530

    directories created

---
+++ src/vrp_basic/src/src/CMakeFiles/vrp_single.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/manikanta/Study/GSoC/pgrouting/src/vrp_basic/src/VRP.c" "/home/manikanta/Study/GSoC/pgrouting/src/vrp_basic/src/src/CMakeFiles/vrp_single.dir/VRP.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/manikanta/Study/GSoC/pgrouting/src/vrp_basic/src/VRP_Solver.cpp" "/home/manikanta/Study/GSoC/pgrouting/src/vrp_basic/src/src/CMakeFiles/vrp_single.dir/VRP_Solver.o"
+  "/home/manikanta/Study/GSoC/pgrouting/src/vrp_basic/src/VRP_core.cpp" "/home/manikanta/Study/GSoC/pgrouting/src/vrp_basic/src/src/CMakeFiles/vrp_single.dir/VRP_core.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ src/vrp_basic/src/src/CMakeFiles/vrp_single.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vrp_single.dir/VRP.o"
+  "CMakeFiles/vrp_single.dir/VRP_core.o"
+  "CMakeFiles/vrp_single.dir/VRP_Solver.o"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/vrp_single.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
